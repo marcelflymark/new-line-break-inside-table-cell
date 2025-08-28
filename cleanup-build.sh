e4b98963fb26f0a6217af2aa7f25fb7c6452b73a
Marcel <marcel@benji.home>
2025-08-28 11:40:01 +0200

    Remove node_modules from Git tracking

--- cleanup-build.sh
+++
@@ -1,4 +0,0 @@
-set -x
-rm -r node_modules
-rm esbuild.config.mjs package.json tsconfig.json package-lock.json  
-
--- updatetoken.sh
+++
@@ -1,1 +0,0 @@
-git remote set-url origin https://marcelflymark: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@github.com/marcelflymark/new-line-break-inside-table-cell.git
